8d36f9b48727a6afb4cb433fe1b3c9bac2058f25
Leonardo <leonardovdg@hotmail.com>
2020-02-05 17:44:51 -0300

    Testingafter git bug, implemented the new Stats System

---
+++ RPG/Assets/Scripts/PlayerUIController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerUIController : MonoBehaviour
+{
+    [SerializeField] GameObject characterC = null;
+    [SerializeField] GameObject characterI = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HitButton()) return;
+    }
+
+    private bool HitButton()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            OpenClose(characterC);
+            return true;
+        }
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            OpenClose(characterI);
+            return true;
+        }
+        return false;
+    }
+
+    private void OpenClose(GameObject win)
+    {
+        win.SetActive(!win.activeSelf);
+    }
+}
