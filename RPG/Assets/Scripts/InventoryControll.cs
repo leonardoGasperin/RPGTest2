aa997f86904280dac313bd1085c4a7a7f6a4074d
Leonardo <leonardovdg@hotmail.com>
2020-02-17 03:12:28 -0300

    Inventory System
    Created inventory system, only need use for real an item and get empty slot when item get less then 0

---
+++ RPG/Assets/Scripts/InventoryControll.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryControll : MonoBehaviour
+{
+    [SerializeField] List<InventorySlotManager> invSlot;
+    [SerializeField] InventorySlotManager slotPrefab = null;
+    [SerializeField] Transform invGrid = null;
+
+    public static InventoryControll instance;
+    public InventorySlotManager selectedSlot;
+
+    private void Start()
+    {
+        instance = this;
+        slotPrefab.SetUpSlot();
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject tempSlot = Instantiate(slotPrefab.gameObject);
+            tempSlot.transform.SetParent(invGrid, false);
+            invSlot.Add(tempSlot.GetComponent<InventorySlotManager>());
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        selectedSlot = null;
+    }
+
+    private void OnDisable()
+    {
+        selectedSlot = null;
+    }
+
+    private void Update()
+    {
+        if(selectedSlot != null && selectedSlot.item != null)
+        {
+            selectedSlot.opt.SetActive(true);
+            if (selectedSlot.item.isWeapon)
+                selectedSlot.buttonEquip.SetActive(true);
+            else
+                selectedSlot.buttonUse.SetActive(true);
+
+            selectedSlot.buttonDrop.SetActive(true);
+        }
+    }
+
+    public void AddItem(PickupItens item)
+    {
+        bool found = false;
+        InventorySlotManager empty = NextEmptySlot();
+
+        if (item.isStack)
+        {
+            foreach (InventorySlotManager slot in invSlot)
+            {
+                if (slot.item != null && slot.item.name == item.gameObject.name)
+                {
+                    slot.item.AddItem();
+                    found = true;
+                }
+            }
+
+            if (!found && empty != null)
+            {
+                empty.item = item;
+            }
+        } else if(empty != null)
+        {
+            empty.item = item;
+        }
+    }
+
+    private InventorySlotManager NextEmptySlot()
+    {
+        InventorySlotManager slotReturn = null;
+        foreach(InventorySlotManager slot in invSlot)
+        {
+            if (slot.item == null)
+            {
+                slotReturn = slot;
+                break;
+            }
+        }
+        return slotReturn;
+    }
+
+}
--- RPG/Assets/Scripts/PickupItens.cs
+++ RPG/Assets/Scripts/PickupItens.cs
@@ -1,15 +1,27 @@
 ﻿/*####Administra itens pegaveis*/
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickupItens : MonoBehaviour, IRayCastable
 {
     [SerializeField] WeaponConfig pickableWeapon = null;//item pegavel
+    public Image icon = null;
+    [SerializeField] InventoryControll inventory = null;
     [SerializeField] float HpRest = 20;
+    [SerializeField] int amount = 0;
     [SerializeField] float respawnT = 5;//tempo de respawn
     [SerializeField] bool isRespawnable = false;//ativaodr do respawn
 
+    public bool isStack = false;
+    
     bool canGet = false;
     
+    public bool isWeapon
+    {
+        get { return pickableWeapon == null ? false : true; }
+    }
+
     private void Update()
     {
         if (GetInRange(GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().transform) && canGet)
@@ -18,24 +30,52 @@ public class PickupItens : MonoBehaviour, IRayCastable
         }
     }
 
+    internal void AddItem(int recived = 1)
+    {
+        amount += recived;
+    }
+    internal void RemoveItem(int used = 1)
+    {
+        amount -= used;
+        if (amount <= 0)
+            Destroy(gameObject);
+    }
+
+    public int GetAmount()
+    {
+        return amount;
+    }
+
     //pega item
     private void PickUp(GameObject other)
     {
         //se for alguma arma
         if (pickableWeapon != null)
         {
-            other.GetComponent<Combat>().EquipWeapon(pickableWeapon);//equipa a arma
+            inventory.AddItem(this);
+            //other.GetComponent<Combat>().EquipWeapon(pickableWeapon);//equipa a arma
             ShowAndHide showHide = GetComponentInParent<ShowAndHide>();//esconde o objeto do scenario
             showHide.Initiate(gameObject, respawnT);//volta depois de tanto tempo
         }
         if (HpRest > 0)//se o item tiver HpRest maior que 0
         {
-            other.GetComponent<Health>().Heal(HpRest);//cura o HP
+            inventory.AddItem(this);
+            //other.GetComponent<Health>().Heal(HpRest);//cura o HP
             ShowAndHide showHide = GetComponentInParent<ShowAndHide>();//esconde o objeto do scenario
             showHide.Initiate(gameObject, respawnT);//volta depois de tanto tempo
         }
         canGet = false;
     }
+
+    public void Use()
+    {
+
+    }
+    public virtual void AfterUse()
+    {
+
+    }
+
     //retorna distancia da arma
     private bool GetInRange(Transform targetT)
     {
@@ -49,8 +89,9 @@ public class PickupItens : MonoBehaviour, IRayCastable
         {
             caller.gameObject.GetComponent<Move>().MoveTo(transform.position, 1, false);
             canGet = true;
+            return true;
         }
-        return true;
+        return false;
     }
     public CursorType GetCursorType()
     {
